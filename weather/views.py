e123420ab081df6840c4b44f6a2856ebafa4acfc
Atya <nushnushka@gmail.com>
2022-09-15 17:58:35 -0300

    Add 'else' clause for the cases when city is not found

--- weather/views.py
+++ weather/views.py
@@ -11,29 +11,33 @@ def index(request):
 
     # cities = City.objects.all()
 
-    weather_data = []
     if request.method == 'POST':
         city = request.POST.get('city')
     else:
         city = 'Saint Petersburg'
-
+    
     weather_request = requests.get(
         f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}'
     )
-    print(weather_request.status_code)
+
     if weather_request.status_code == 200:
         response = weather_request.json()
 
-        city_weather = {
+        weather_data = [{
             'city': str(city).title(),
             'temperature': int(response['main']['temp']),
             'feels_like': int(response['main']['feels_like']),
             'description': str(response['weather'][0]['description']).title(),
             'icon': response['weather'][0]['icon'],
-        }
-
-        weather_data.append(city_weather)
-
-    form = CityForm()
-    context = {'weather_data': weather_data, 'form': form}
-    return render(request, 'weather/weather.html', context)
+            'country':  response['sys']['country'],
+        }]
+          
+        
+        form = CityForm()
+        context = {'weather_data': weather_data, 'form': form}
+        return render(request, 'weather/weather.html', context)
+    
+    else:
+        form = CityForm()
+        context = {'form': form}
+        return render(request, 'weather/weather.html', context)
