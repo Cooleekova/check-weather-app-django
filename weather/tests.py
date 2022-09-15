33bb47a72ef5002b6f79b1a975bdad8ed68cf3e0
Atya <nushnushka@gmail.com>
2022-09-15 17:54:36 -0300

    Add new tests for testing index view

--- weather/tests.py
+++ weather/tests.py
@@ -1,6 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse, resolve
-from views import index
+from weather.views import index
 
 # Create your tests here.
 
@@ -11,4 +11,21 @@ class TestUrls(TestCase):
         url = reverse('index')
         self.assertEquals(url, '/')
         self.assertEquals(resolve(url).func, index)
+        
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_GET(self):
+        response = self.client.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('weather/weather.html')
+
+    def test_index_POST(self):
+        response = self.client.post(reverse('index'), {'city': "New York"})
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.context['weather_data'][0]['city'], "New York")
+        self.assertTemplateUsed('weather/weather.html')
         
