7b1c1754835f8b5b264e8998720f8d75ada7ee85
Tatiana <74092403+Cooleekova@users.noreply.github.com>
2022-09-15 16:43:57 -0300

    Add test for index url

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from views import index
 
 # Create your tests here.
+
+
+class TestUrls(TestCase):
+
+    def test_index_url_resolves(self):
+        url = reverse('index')
+        self.assertEquals(url, '/')
+        self.assertEquals(resolve(url).func, index)
+        
